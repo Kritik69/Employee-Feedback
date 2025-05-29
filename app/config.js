97dd278f10bcfea51c283842dd9f9d087f727e57
Kritik69 <kritikjambusariya@gmail.com>
2025-05-30 01:48:17 +0530

    live db

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
-// export const API_URL = 'https://employee-feedback-server-315893334095.europe-west1.run.app';
-// export const SOCKET_URL = 'https://employee-feedback-server-315893334095.europe-west1.run.app';
-export const API_URL = 'http://localhost:5000';
-export const SOCKET_URL = 'http://localhost:5000';
+export const API_URL = 'https://employee-feedback-server-315893334095.europe-west1.run.app';
+export const SOCKET_URL = 'https://employee-feedback-server-315893334095.europe-west1.run.app';
+// export const API_URL = 'http://localhost:5000';
+// export const SOCKET_URL = 'http://localhost:5000';
 
 // Add other configuration variables as needed
 export const config = {
